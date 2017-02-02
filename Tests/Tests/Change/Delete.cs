5cfd1400c9f2b8e1f8c4104c688cd60f1df019df
Talles L <talleslasmar@gmail.com>
2017-02-02 17:18:19 -0200

    some more unit tests

--- Tests/Tests/Change/Delete.cs
+++ Tests/Tests/Change/Delete.cs
@@ -1,6 +1,7 @@
 ﻿namespace QueryLibrary.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
     using System.Linq;
 
     [TestClass]
@@ -15,6 +16,33 @@
             var expected = Game.StarFoxGames.Where(g => g.Id != 4).ToArray();
 
             CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ManualDelete()
+        {
+            Func<Game[]> actualGames = () => Query.Select<Game>(GameSelect).ToArray();
+            Func<Console[]> actualConsoles = () => Query.Select<Console>(ConsoleSelect).ToArray();
+
+            var query = ManualQuery;
+
+            query.Change("DELETE FROM Game WHERE ConsoleId IN (SELECT Id FROM Console WHERE PortableConsole = 1)");
+            query.Change("DELETE FROM Console WHERE PortableConsole = 1");
+
+            CollectionAssert.AreEqual(Game.StarFoxGames.ToArray(), actualGames());
+            CollectionAssert.AreEqual(Console.NintendoConsoles.ToArray(), actualConsoles());
+
+            query.Close();
+
+            CollectionAssert.AreEqual(
+                Game.StarFoxGames.Where(g => !g.Console.PortableConsole).ToArray(),
+                actualGames()
+            );
+
+            CollectionAssert.AreEqual(
+                Console.NintendoConsoles.Where(c => !c.PortableConsole).ToArray(),
+                actualConsoles()
+            );
         }
     }
 }
